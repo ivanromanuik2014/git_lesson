a920093fde961f20e317c4873d5eaaa9d0a2c08c
Ivan Romaniuk <ivanromanuk2014@gmail.com>
2024-02-15 15:52:45 +0200

    Task for first lesson by python core

---
+++ 01.1 PRACTICAL TASK1.py
@@ -0,0 +1,11 @@
+#Request user information
+
+user_name = input('What is your name ')
+user_age = input('How old are you? ')
+user_location = input('Where do you live ')
+
+#Output user information
+
+print(f'Hello, {user_name}')
+print(f'Your age is, {user_age}')
+print(f'You live in, {user_location}')
---
+++ 01.2 PRACTICAL TASK2.py
@@ -0,0 +1,12 @@
+# Receiving numbers A and B from user
+number_A = int(input('Please entered your first number: '))
+number_B = int(input('Please entered your second number: '))
+
+# Culculated A and B according to the given conditions
+print(f'A + B = {number_A + number_B};')
+print(f'A - B = {number_A - number_B};')
+print(f'A * B = {number_A * number_B};')
+print(f'A / B = {number_A / number_B};')
+print(f'A ** B = {number_A ** number_B};')
+print(f'A // B = {number_A // number_B};')
+print(f'A % B = {number_A % number_B};')
